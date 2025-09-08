230c583e4ae3e55a25057256a36ee1b2fd58ee80
Michael Reape <michaelreape@gmail.com>
2025-09-08 12:19:13 +0100

    merge fix

--- Assets/Scripts/Spawner Scripts/ObjectLoader.cs
+++ Assets/Scripts/Spawner Scripts/ObjectLoader.cs
@@ -50,7 +50,6 @@ public class ObjectLoader : MonoBehaviour
             parent.transform.position = position;
             //add a box collider
             BoxCollider boxCollider = parent.AddComponent<BoxCollider>();
-            RecalculateBoundsFromChildren(parent, boxCollider);
             //update the object data structure for saving
             ObjectSaver.ObjectSaverInstance.UpdateData(prompt, parent.transform.position, parent.transform.rotation, parent.transform.localScale);
         }
@@ -61,21 +60,6 @@ public class ObjectLoader : MonoBehaviour
         }
     }
 
-    private void RecalculateBoundsFromChildren(GameObject parent, BoxCollider boxCollider)
-    {
-        var meshRenderers = parent.GetComponentsInChildren<MeshRenderer>();
-
-        //start with the first renderer's bounds
-        Bounds combinedBounds = meshRenderers[0].bounds;
-        //encapsulate all child mesh bounds
-        for (int i = 1; i < meshRenderers.Length; i++)
-        {
-            combinedBounds.Encapsulate(meshRenderers[i].bounds);
-        }
-        //BoxCollider center is in local space, so convert from world to local
-        boxCollider.center = parent.transform.InverseTransformPoint(combinedBounds.center);
-        boxCollider.size = combinedBounds.size;
-    }
 
     //object loader from json
     public async void LoadObjectFromSave(ObjectData objectData)
@@ -102,8 +86,24 @@ public class ObjectLoader : MonoBehaviour
             parent.transform.localScale = objectData.scale;
             //add a box collider
             BoxCollider boxCollider = parent.AddComponent<BoxCollider>();
-            RecalculateBoundsFromChildren(parent, boxCollider);
             ObjectSaver.ObjectSaverInstance.UpdateData(objectData.name, objectData.position, objectData.rotation, objectData.scale);
         }
     }
+
+
+    //private void RecalculateBoundsFromChildren(GameObject parent, BoxCollider boxCollider)
+    //{
+    //    var meshRenderers = parent.GetComponentsInChildren<MeshRenderer>();
+
+    //    //start with the first renderer's bounds
+    //    Bounds combinedBounds = meshRenderers[0].bounds;
+    //    //encapsulate all child mesh bounds
+    //    for (int i = 1; i < meshRenderers.Length; i++)
+    //    {
+    //        combinedBounds.Encapsulate(meshRenderers[i].bounds);
+    //    }
+    //    //BoxCollider center is in local space, so convert from world to local
+    //    boxCollider.center = parent.transform.InverseTransformPoint(combinedBounds.center);
+    //    boxCollider.size = combinedBounds.size;
+    //}
 }
